bd767b04e60da2603c57df019dfdabdf4e021e9a
Иван <yarcrazy@yandex.ru>
2023-05-09 23:26:26 +0800

    [update] п.4

---
+++ common/models/Article.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $code
+ * @property string|null $content
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'code'], 'required'],
+            [['content'], 'string'],
+            [['title', 'code'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'code' => 'Code',
+            'content' => 'Content',
+        ];
+    }
+}
