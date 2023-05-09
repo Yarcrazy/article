c0bb34193e7a0b09608a1651c9abffe6ad295aad
Иван <yarcrazy@yandex.ru>
2023-05-09 23:30:18 +0800

    [update] п.8

---
+++ frontend/views/article/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use common\models\Article;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Articles';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="article-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Article', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'code',
+            'content:ntext',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Article $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
